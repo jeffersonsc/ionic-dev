8c56cd7a5f33f02cf18e6d9c483ec641e1360624
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-06-08 14:33:47 -0300

    Add config to auth
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- www/controllers/app.controller.js
+++ www/controllers/app.controller.js
@@ -1,3 +1,23 @@
-App.controller('AppController', function ($scope) {
-  
+App.controller('AppController', function ($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
+  $scope.username = AuthService.username;
+
+  $scope.$on(AUTH_EVENTS.notAuthorized, function(event){
+    var alertPopup = $ionicPopup.alert({
+      title: 'Not authorized',
+      template: 'You not authorized!'
+    });
+  });
+
+  $scope.$on(AUTH_EVENTS.notAuthenticated, function(event){
+    AuthService.logout();
+    $state.go('login');
+    var alertPopup = $ionicPopup.alert({
+      title: 'Not authenticated',
+      template: 'You not authenticated.'
+    });
+  });
+
+  $scope.setCurrentUsername = function(name){
+    $scope.username = name;
+  };
 })
--- www/services/service.js
+++ www/services/service.js
@@ -1,39 +1,75 @@
-App.service('AuthService', function($q, $http) {
-  var LOCAL_TOKEN_KEY = 'token';
-  var username = '';
-  var isAuthenticated = false;
-  var authToken;
+App
+  .service('AuthService', function($q, $http) {
+    var LOCAL_TOKEN_KEY = 'token';
+    var username = '';
+    var isAuthenticated = false;
+    var authToken;
 
-  function loadUserCredentials(){
-    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
-    if (token){
+    function loadUserCredentials(){
+      var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+      if (token){
+        useCredentials(token);
+      }
+    }
+
+    function storeUserCredentials(token) {
+      window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
       useCredentials(token);
     }
-  }
 
-  function storeUserCredentials(token) {
-    window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
-    useCredentials(token);
-  }
+    function useCredentials(token) {
+      username = token.split('.')[0];
+      isAuthenticated = true;
+      authToken = token;
 
-  function useCredentials(token) {
-    username = token.split('.')[0];
-    isAuthenticated = true;
-    authToken = token;
+      setHttpHeader(token);
+    }
 
-    setHttpHeader(token);
-  }
+    function destroyUserCredentials(token) {
+      authToken = undefined;
+      username = '';
+      isAuthenticated = false
+      setHttpHeader(authToken);
+      window.localStorage.removeItem(LOCAL_TOKEN_KEY)
+    }
 
-  function destroyUserCredentials(token) {
-    authToken = undefined;
-    username = '';
-    isAuthenticated = false
-    setHttpHeader(authToken);
-    window.localStorage.removeItem(LOCAL_TOKEN_KEY)
-  }
+    function setHttpHeader(token){
+       // Set the token as header for your requests!
+      $http.defaults.headers.common['X-Auth-Token'] = token;
+    }
 
-  function setHttpHeader(token){
-     // Set the token as header for your requests!
-    $http.defaults.headers.common['X-Auth-Token'] = token;
-  }
-})
+    var login = function(email, password){
+      return $q(function(resolve, reject) {
+        //Use $http for request server to session login
+        if (email === 'teste@teste.com' && password === '12345678') {
+          storeUserCredentials(email + 'hsddsjhdhgaaseuqrheohcdb');
+          resolve('Login Success!')
+        }else(
+          reject('')
+        )
+      })
+    }
+
+    var logout = function(){
+      destroyUserCredentials();
+    }
+
+    return {
+      login: login,
+      logout: logout,
+      isAuthenticated: function() { return isAuthenticated; },
+      username: function() { return username; }
+    }
+  })
+
+  .factory('AuthInterceptor', function($rootScope, $q, AUTH_EVENTS){
+    return {
+      responseError: function(response) {
+        $rootScope.$broadcast({
+          401: AUTH_EVENTS.notAuthenticated,
+          403: AUTH_EVENTS.notAuthorized
+        }, [response.status], response);
+        return $q.reject(response);
+      }
+    };
+  })
